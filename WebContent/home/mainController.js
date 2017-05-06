c218a2073ee28bd787e8a1bbb9080a62178d85e4
saleem <saleem@saleem-PC>
2017-05-06 15:18:55 +0530

    resources

--- WebContent/home/mainController.js
+++ WebContent/home/mainController.js
@@ -1,4 +1,5 @@
-	// create the controller and inject Angular's $scope
+
+// create the controller and inject Angular's $scope
 	app.controller('mainController', function($scope) {
 		// create a message to display in our view
 		console.log("main controller")
